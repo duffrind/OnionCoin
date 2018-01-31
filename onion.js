ca4c593033f17eb2086f2fafeab70bcb5cc03ded
David Duffrin <david.duffrin15@ncf.edu>
2018-01-30 20:01:35 -0500

    added custom donation amount code - TODO merge buttons

--- onion.js
+++ onion.js
@@ -1,8 +1,10 @@
 var onions = document.getElementById('coinAmount')
 var cost = document.getElementById('price')
+var amount = document.getElementById('amount')
 
 onions.addEventListener('change', updatePrice)
 
 function updatePrice () {
   cost.value = onions.value * 5
+  amount.value = onions.value * 5
 }
