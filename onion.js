d747cc266cdb783c03fbd0d8bd1712f033fc174a
David Duffrin <david.duffrin15@ncf.edu>
2018-02-06 16:11:44 -0500

    note works - keep?

--- onion.js
+++ onion.js
@@ -1,10 +1,18 @@
 var onions = document.getElementById('coinAmount')
 var cost = document.getElementById('price')
 var amount = document.getElementById('amount')
+var wallet = document.getElementById('wallet')
+var item = document.getElementById('item_number')
 
+wallet.addEventListener('change', updateWallet)
 onions.addEventListener('change', updatePrice)
 
 function updatePrice () {
-  cost.value = onions.value * 5
-  amount.value = onions.value * 5
+  cost.value = (parseFloat(onions.value * 5).toPrecision(2))
+  amount.value = (parseFloat(onions.value * 5).toPrecision(2))
+}
+
+function updateWallet () {
+  item.value = wallet.value
+  // document.getElementById('item_name').value = wallet.value
 }
